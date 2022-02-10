a45b9822ca775b7afb6c7fd999458448e04c8d3f
Jake Mullins <jmullins2003@outlook.com>
2022-02-10 16:16:35 -0700

    js test

---
+++ www/lab2/script.js
@@ -0,0 +1,7 @@
+document.getElementById("weatherSubmit").addEventListener("click", function(event) {
+    event.preventDefault();
+    const value = document.getElementById("weatherInput").value;
+    if (value === "")
+        return;
+    console.log(value);
+});
