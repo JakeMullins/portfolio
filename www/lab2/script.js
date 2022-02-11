bd39aea640899e000690e6aa29105130f002cb1f
Jake Mullins <jmullins2003@outlook.com>
2022-02-11 12:07:11 -0700

    Learn Ajax

--- www/lab2/script.js
+++ www/lab2/script.js
@@ -4,4 +4,12 @@ document.getElementById("weatherSubmit").addEventListener("click", function(even
     if (value === "")
         return;
     console.log(value);
+    const url = "http://api.openweathermap.org/data/2.5/weather?q=" + value + ",US&units=imperial" + "&APPID=b545ceab5f400c3f066fb28c8c7bbaef";
+    fetch(url)
+        .then((response) => {
+            return response.json();
+        })
+        .then((json) => {
+            console.log(json);
+        });
 });
