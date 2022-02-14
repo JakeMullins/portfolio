5789b77d7c78aa9b1dbf401b6d0a1bab15c9db56
Jake Mullins <jmullins2003@outlook.com>
2022-02-14 14:43:50 -0700

    Weather app functionality added

--- www/lab2/script.js
+++ www/lab2/script.js
@@ -13,21 +13,22 @@ document.getElementById("weatherSubmit").addEventListener("click", (event) => {
             return response.json();
         })
         .then((json) => {
-            console.log(json);
-
             let results = "";
-            results += '<h2>Weather in ' + json.name + "</h2>";
+            results += '<h2>Weather in ' + json.name + " right now is: </h2>";
             for (let i = 0; i < json.weather.length; i++) {
                 results += '<img src="http://openweathermap.org/img/w/' + json.weather[i].icon + '.png"/>';
             }
-            results += '<h2>' + Math.floor(json.main.temp) + " &deg;F</h2>"
+            results += '<h2>' + json.main.temp + " &deg;F</h2>";
+            results += '<h2>' + json.main.humidity + " % humidity</h2>";
+            results += '<h2>' + json.main.pressure + " millibars</h2>";
             results += "<p>"
             for (let i = 0; i < json.weather.length; i++) {
                 results += json.weather[i].description
-                if (i !== json.weather.length - 1)
+                if (i !== json.weather.length - 1) {
                     results += ", "
+                }
             }
-            results += "</p>";
+            results += "</p><hr>";
             document.getElementById("weatherResults").innerHTML = results;
         })
 
@@ -37,11 +38,14 @@ document.getElementById("weatherSubmit").addEventListener("click", (event) => {
             return response.json();
         })
         .then((json) => {
+            console.log(json);
             let forecast = "";
             for (let i = 0; i < json.list.length; i++) {
                 forecast += "<h2>" + moment(json.list[i].dt_txt).format('MMMM Do YYYY, h:mm:ss a') + "</h2>";
-                forecast += "<p>Temperature: " + json.list[i].main.temp + "</p>";
-                forecast += '<img src="http://openweathermap.org/img/w/' + json.list[i].weather[0].icon + '.png"/>'
+                forecast += "<p>Temperature: " + Math.floor(json.list[i].main.temp) + "</p>";
+                forecast += "<p>Humidity: " + json.list[i].main.humidity + "% </p>";
+                forecast += "<p>Pressure: " + json.list[i].main.pressure + " millibars</p>";
+                forecast += '<img src="http://openweathermap.org/img/w/' + json.list[i].weather[0].icon + '.png"/>';
             }
             document.getElementById("forecastResults").innerHTML = forecast;
         })
