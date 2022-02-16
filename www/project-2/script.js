228e567b35ccae4f1b1da32aa0628bd7cee13809
Jake Mullins <jmullins2003@outlook.com>
2022-02-16 12:39:26 -0700

    Almost had to wipe my computer ain't no way I'm not commiting every 3 seconds

---
+++ www/project-2/script.js
@@ -0,0 +1,148 @@
+// On document change generate random lesson pulling from lessons object
+let lessons = {
+    "0": {
+        "letters": "ك ش",
+        "lessonText": ""
+    },
+    "1": {
+        "letters": "س م",
+        "lessonText": ""
+    },
+    "2": {
+        "letters": "ن ي",
+        "lessonText": ""
+    },
+    "3": {
+        "letters": "ت ب",
+        "lessonText": ""
+    },
+    "4": {
+        "letters": "ا ل",
+        "lessonText": ""
+    },
+    "5": {
+        "letters": "د ض",
+        "lessonText": ""
+    },
+    "6": {
+        "letters": "ج ص",
+        "lessonText": ""
+    },
+    "7": {
+        "letters": "ح ث",
+        "lessonText": ""
+    },
+    "8": {
+        "letters": "خ ق",
+        "lessonText": ""
+    },
+    "9": {
+        "letters": "ه ف",
+        "lessonText": ""
+    },
+    "10": {
+        "letters": "ئ ظ",
+        "lessonText": ""
+    },
+    "11": {
+        "letters": "ز ء",
+        "lessonText": ""
+    },
+    "12": {
+        "letters": "و ؤ",
+        "lessonText": ""
+    },
+    "13": {
+        "letters": "ة ر",
+        "lessonText": ""
+    },
+    "14": {
+        "letters": "ى لا",
+        "lessonText": ""
+    }
+}
+
+function generateLessons() {
+
+    // Modifies all lesson text entries in each object
+    Object.entries(lessons).forEach(([key, value]) => {
+        let currLessonText = "";
+
+        letters = value.letters.split(' ');
+
+        let l1 = letters[0];
+        let l2 = letters[1];
+
+        // Add more er
+        currLessonText += repeatConsecutiveLetter(l1, 5) + " " + repeatConsecutiveLetter(l2, 5) + "\n";
+        currLessonText += repeatAlternatingLetters(l1, l2, 5) + " " + repeatAlternatingLetters(l2, l1, 5) + "\n";
+        currLessonText += repeatRandomLetters(l1, l2, 8) + " " + repeatRandomLetters(l1, l2, 8) + "\n";
+        currLessonText += repeatRandomLetters(l1, l2, 4) + " " + repeatRandomLetters(l1, l2, 4) + " " + repeatRandomLetters(l1, l2, 4) + " " + repeatRandomLetters(l1, l2, 4);
+
+        value.lessonText = currLessonText;
+    })
+
+    document.getElementById("lessonSelectDropdown").innerHTML = generateLessonMenu();
+}
+
+function repeatConsecutiveLetter(letter, repeatNum) {
+    let result = "";
+
+    for (let i = 0; i < repeatNum; i++) {
+        result += letter;
+    }
+
+    return result;
+}
+
+function repeatAlternatingLetters(l1, l2, repeatNum) {
+    let result = "";
+
+    for (let i = 0; i < repeatNum; i++) {
+        result += l1 + l2;
+    }
+
+    return result;
+}
+
+function repeatRandomLetters(l1, l2, repeatNum) {
+    let result = "";
+
+    for (let i = 0; i < repeatNum; i++) {
+        let rand = Math.round(Math.random());
+
+        if (rand === 1) {
+            result += l1;
+        } else if (rand === 0) {
+            result += l2;
+        }
+    }
+    return result;
+}
+
+function generateLessonMenu() {
+    let html = "<option value>Select a lesson</option>\n";
+    Object.entries(lessons).forEach(([key, value]) => {
+        html += "<option value=" + key + ">" + value.letters + "</option>\n";
+    })
+    return html;
+}
+
+document.getElementById("generateLessonButton").addEventListener("click", (event) => {
+    generateLessons();
+})
+
+
+document.getElementById("lessonSelectDropdown").onchange = () => {
+    lessonIndex = document.getElementById("lessonSelectDropdown").value;
+    lessonText = lessons[lessonIndex].lessonText;
+    renderLesson(lessonText);
+}
+
+// Tokenize then output lesson
+function renderLesson(lessonText) {
+    tokens = lessonText.split(' ');
+    tokens.forEach((value) => {
+        console.log(value);
+    })
+}
